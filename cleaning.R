d37e34f94873287c61bae982c89e468b391a5d2b
ZYYMBP <zhangyuyang0515@gmail.com>
2022-05-16 16:40:38 -0400

    adding coments

--- cleaning.R
+++ cleaning.R
@@ -1,10 +1,12 @@
 cleaning <- function ()
         {
                 library(dplyr)
+        ## getting data 
         url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(url,"./data.zip",method = "curl")
         unzip("./data.zip")
         
+        ## read data into tables with new col names 
         features <- read.table("./UCI HAR Dataset/features.txt", col.names = c("n","functions"))
         activity <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("label", "activity"))
         subtest <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -13,12 +15,17 @@ cleaning <- function ()
         subtrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
         xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
         ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "label")
+       
+        ## merge data 
         x<- rbind(xtrain,xtest)
         y<- rbind(ytrain,ytest)
         sub<- rbind(subtrain,subtest)
         all<- cbind(sub,y,x)
+        
+        ## get only the mean and std festures 
         mean_std_only <- select(all,subject, label, contains("mean"), contains("std"))
-        ##mean_std_only <- activity[mean_std_only$label,2]
+
+        ## rename col with better names 
         names(mean_std_only)[2] = "activity"
         names(mean_std_only)<-gsub("Acc", "Accelerometer", names(mean_std_only))
         names(mean_std_only)<-gsub("Gyro", "Gyroscope", names(mean_std_only))
@@ -31,10 +38,12 @@ cleaning <- function ()
         names(mean_std_only)<-gsub("-std()", " STD", names(mean_std_only), ignore.case = TRUE)
         names(mean_std_only)<-gsub("-freq()", " Frequency", names(mean_std_only), ignore.case = TRUE)
 
+        ## arrange the table and calculate the mean
         final <- mean_std_only %>% group_by(subject,activity)
-        
         summarize_all(final,funs(mean))
         final$activity <- activity[final$activity,2]
+        
+        ## output data 
         write.table(final,"final.txt",row.name=FALSE)
 
 }
